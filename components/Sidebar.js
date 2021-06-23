0a53aa5fb116c553951518f49786723f733b6b64
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-06-23 17:00:41 +1200

    Head of Sidebar done

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,70 @@
+import styled from 'styled-components'
+import { Avatar, IconButton } from '@material-ui/core'
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 function Sidebar() {
     return (
-        <div>
+        <Container>
+            <Header>
+                <UserAvatar />
 
-        </div>
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                </IconsContainer>
+
+            </Header>
+
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder='Seatch in Chat' />
+            </Search>
+        </Container>
     )
 }
 
 export default Sidebar
+
+const Container = styled.div`
+`;
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconsContainer = styled.div`
+
+`;
+
+const Search = styled.div`
+
+`;
+
+const SearchInput = styled.input`
+
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>WhatsApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>WhatsApp 2.0</h1>
+      <Sidebar />
 
     </div>
   )
