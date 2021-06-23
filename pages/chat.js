bd119ed2db6f4f662d3dd5715ed7dbf077406646
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-06-23 16:39:38 +1200

    Added page routing

---
+++ pages/chat.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+function chat() {
+    return (
+        <div>
+            <Head>
+                <title>WhatsApp</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Chat</h1>
+        </div>
+    )
+}
+
+export default chat
